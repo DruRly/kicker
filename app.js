1c891c64982b2eca6df65421d253935a860f5704
Dru Riley <drurly@gmail.com>
2014-04-23 07:53:34 -0400

    Create MVP: It sets multiple notes
    TODO:
    Change options name
    Operate on same object (don't set multiple notes)
    Use a slider instead
    Create design
    Share

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-window.addEventListener('load', init, false);
+function KickerController($scope) {
+  $scope.colors = [
+    {name:100, value: 100},
+    {name:1000, value: 1000},
+    {name:5000, value: 5000},
+    {name:15000, value: 15000},
+    {name:20000, value: 20000}
+  ];
 
-function init() {
-  try {
+  $scope.setFrequency = function() {
     var context = new webkitAudioContext();
     var oscillator = context.createOscillator();
     oscillator.type = 0;
-    oscillator.frequency.value = 2000;
+    console.log($scope.frequency.value);
+    oscillator.frequency.value = $scope.frequency.value;
     oscillator.connect(context.destination);
-    oscillator.noteOn(5000);
-  }
-  catch(e) {
-    alert('Web Audio API is not supported in this browser');
+    oscillator.noteOn(0);
   }
 }
