1ff7df4d30cd0a3b8a98ff76c71e59a34df6531b
Dru Riley <drurly@gmail.com>
2014-04-24 07:04:49 -0400

    Get local copy of bootstrap and refactor Audio singleton

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
-var demoModule = angular.module("demo", ['rzModule'])
+var kicker = angular.module("kicker", ['rzModule'])
 
 function KickerController($scope) {
   $scope.frequency = 200;
-
-  var Audio = (function () {
-
-    var instance;
-
-    function createInstance() {
-      var context = new webkitAudioContext();
-      var oscillator = context.createOscillator();
-      oscillator.type = 0;
-      oscillator.frequency = 0;
-      oscillator.connect(context.destination);
-      oscillator.noteOn(0);
-      return oscillator;
-    }
-
-    return {
-      getInstance: function () {
-        if (!instance) {
-          instance = createInstance();
-        }
-
-        return instance;
-      }
-    };
-  })();
 
   $scope.$watch('frequency', function(){
     $scope.setFrequency();
@@ -36,3 +11,28 @@ function KickerController($scope) {
     Audio.getInstance().frequency.value = $scope.frequency;
   }
 };
+
+var Audio = (function () {
+
+  var instance;
+
+  function createInstance() {
+    var context = new webkitAudioContext();
+    var oscillator = context.createOscillator();
+    oscillator.type = 0;
+    oscillator.frequency = 0;
+    oscillator.connect(context.destination);
+    oscillator.noteOn(0);
+    return oscillator;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+
+      return instance;
+    }
+  };
+})();
